bcdad2805c9b7e360ac863fe740d19bce2d9a5c6
unknown <satrgomez7@gmail.com>
2023-02-10 11:18:41 -0400

    redirect onload to /shopping

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ import Layout from "../components/Layout";
 import { Box } from "@mui/material";
 
 const Index = () => {
+  useEffect(() => {
+    document.location.href = '/shopping'
+  }, [])
   return (
     <Layout>
       <Box className="flex justify-center items-center flex-col mt-[30px]">
